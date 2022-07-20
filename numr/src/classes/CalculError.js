5f1a45752183c46be139b332f5464dbfe68dfaf0
Amir Anglo <angloamir@gmail.com>
2022-07-20 22:58:06 +0200

    Fix CalculError

--- numr/src/classes/CalculError.js
+++ numr/src/classes/CalculError.js
@@ -2,4 +2,6 @@ export class CalculError {
     constructor(args) {
       throw new Error(`${args.type}: ${args.message}`);
     };
+
+    
 }; 
--- numr/src/functions/Calcul.js
+++ numr/src/functions/Calcul.js
@@ -3,39 +3,43 @@ import { CalculError } from "../../index.js";
 export var Calcul = {
 
     add(num1, num2) {
-        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'})
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ type: 'SyntaxError', message: 'Invalid Number' })
         return num1 + num2;
     },
 
     subtract(num1, num2) {
-        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'})
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ type: 'SyntaxError', message: 'Invalid Number'})
         return num1 - num2;
     },
 
     multiply(num1, num2) {
-        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'})
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ type: 'SyntaxError', message: 'Invalid Number'})
         return num1 * num2;
     },
 
     divide(num1, num2) {
-        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'})
-        return num1 / num2;
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ type: 'SyntaxError', message: 'Invalid Number'})
+    },
+
+    difference(num1, num2) {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new CalculError({ type: 'SyntaxError', message: 'Invalid Number'})
+
     },
 
     getRandomNumber(min, max, param) {
-        if (typeof min !== 'number' || typeof max !== 'number' ) throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'})
+        if (typeof min !== 'number' || typeof max !== 'number' ) throw new CalculError({ type: 'SyntaxError', message: 'Invalid Number'})
             switch (param) {
               case 'decimal':
                 return Math.random() * (max - min) + min;
               case 'round':
                 return Math.floor(Math.random() * (max - min) + min);
               default: 
-                throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'});
+                throw new CalculError({ type: 'TypeError', message: 'Invalid Type'});
             }
     },
 
     getSignNumber(num, param) {
-        if (typeof num !== 'number') throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'})
+        if (typeof num !== 'number') throw new CalculError({ type: 'SyntaxError', message: 'Invalid Number'})
 
         let bool = Math.sign(num)
 
@@ -52,8 +56,9 @@ export var Calcul = {
             case 'number':
               return bool;
             default: 
-              throw new CalculError({ message: 'SyntaxError', type: 'Invalid syntax'});
-          }
-            
-    }
+            throw new CalculError({ type: 'TypeError', message: 'Invalid Number'})
+          }        
+    },
+
+
 }
